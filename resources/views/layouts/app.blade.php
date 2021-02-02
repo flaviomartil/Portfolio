75264c1ebbfa00b5e3bf0f61f16878eb88200d6c
flaviomartil <flaviomartil5@gmail.com>
2021-02-02 10:17:15 -0300

    Correção layout admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,8 +51,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                           <p><a href="{{ route('login') }}">Login</a></p>
+                            <p><a href="{{ route('register') }}">Register</a></p>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
